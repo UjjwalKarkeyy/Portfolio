61a58b306ef2e377916c2743cedf204038a7714d
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-11-18 20:05:49 -0800

    ...

--- portfolioFrontend/src/pages/Home.jsx
+++ portfolioFrontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // src/pages/Home.jsx
 import homePic from "../assets/home_pic.jpg";
-import ujjwalCV from "../assets/Ujjwal_Karki_CV.pdf"
+import ujjwalResume from "../assets/Ujjwal_Karki_resume.pdf"
 
 function Home() {
   return (
@@ -21,8 +21,8 @@ function Home() {
             Feel free to explore - and reach out if you ever want to collaborate, talk tech,
             or even team up for a gaming session.
           </p>
-          <a href={ujjwalCV} download className="download-btn">
-            Download CV
+          <a href={ujjwalResume} download className="download-btn">
+            Download Resume
           </a>
         </div>
 
